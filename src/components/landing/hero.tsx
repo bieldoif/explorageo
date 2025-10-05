b6f6aad50fc2eadce2ccb6a99042835389eb319f
Pedro Gabriel <pgr85206@gmail.com>
2025-10-05 16:49:04 +0000

    retire essa fumaça branca que esta abaixo do botao quero transformar min

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -28,7 +28,6 @@ export function Hero() {
           </a>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent" />
     </section>
   );
 }
